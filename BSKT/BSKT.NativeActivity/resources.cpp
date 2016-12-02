04bcb8de7a0686d1aba1a09466ba5f9f4a7915aa
francescoz <fz.francescoz@gmail.com>
2016-12-02 12:53:38 +0100

    Add test mesh

--- BSKT/BSKT.NativeActivity/resources.cpp
+++ BSKT/BSKT.NativeActivity/resources.cpp
@@ -4,19 +4,10 @@
 namespace BSKT {
 
 	static GLuint createProgram(const char*, const char*);
+	static GLuint createProgram(const GLProgramSource);
 	static GLuint createShader(GLenum, const char*);
 
-	Resources::Resources(GLuint program) : program(program) { };
-
-	void Resources::destroy() {
-		glDeleteProgram(program);
-	}
-
-	Resources createResources() {
-		return Resources(createProgram("ca", "zzo"));
-	}
-
-	static GLuint createShader(GLenum type, const char* source) {
+	static GLuint createShader(GLenum type, const char *source) {
 		GLuint shader;
 		if ((shader = glCreateShader(type)) == 0) {
 			LOGE("No shader created");
@@ -44,7 +35,7 @@ namespace BSKT {
 		return shader;
 	}
 
-	static GLuint createProgram(const char* vertexSource, const char* fragmentSource) {
+	static GLuint createProgram(const char *vertexSource, const char *fragmentSource) {
 		GLuint program;
 		if ((program = glCreateProgram()) == 0) {
 			LOGE("No program created");
@@ -73,7 +64,77 @@ namespace BSKT {
 				return NULL_RES;
 			}
 		}
+		glDetachShader(program, vertexShader);
+		glDetachShader(program, fragmentShader);
+		glDeleteShader(vertexShader);
+		glDeleteShader(fragmentShader);
 		return program;
+	}
+
+	static GLuint createProgram(const GLProgramSource source) {
+		return createProgram(source.vertexShader, source.vertexShader);
+	}
+
+	GLProgramSource::GLProgramSource() { }
+
+	GLProgramSource::GLProgramSource(AAssetManager *assets, const char *name) {
+		const char *path[] = { SHADERS_DIR, name, VERTEX_SHADER_FILE };
+		vertexShader = parseFile(path, 3, assets);
+		path[2] = FRAGMENT_SHADER_FILE;
+		fragmentShader = parseFile(path, 3, assets);
+	}
+
+	Mesh::Mesh() { }
+
+	Mesh::Mesh(AAssetManager *assets, const char *name) {
+		const char *path[] = { MESHES_DIR, name };
+		const char *str = parseFile(path, 2, assets);
+		int len = strlen(str);
+		count = 1;
+		for (int i = 0; i < len; i++)
+			if (str[i] == MESH_VERT_SEP)
+				count++;
+		vertices = new float[count];
+		for (int i = 0; i < len; i++) {
+			char *end;
+			vertices[i] = strtof(str, &end);
+			str = end + 1;
+		}
+	}
+
+	Assets::Assets(GLProgramSource program) { 
+		programSource = program;
+	}
+
+	GLResources::GLResources(Assets assets) {
+		program = createProgram(assets.programSource);
+	}
+
+	void GLResources::destroy() {
+		glDeleteProgram(program);
+	}
+
+	char* parseFile(const char *path, AAssetManager *assets) {
+		AAsset* asset = AAssetManager_open(assets, path, AASSET_MODE_BUFFER);
+		off_t len = AAsset_getLength(asset);
+		char *dst = new char[len + 1];
+		AAsset_read(asset, dst, len);
+		AAsset_close(asset);
+		dst[len] = '\0';
+		return dst;
+	}
+
+	char* parseFile(const char **path, int count, AAssetManager *assets) {
+		int len = count;
+		for (int i = 0; i < count; i++) 
+			len += strlen(path[i]);
+		char *str = new char[len];
+		strcpy(str, *path);
+		for (int i = 1; i < count; i++) {
+			strcat(str, FILE_SEP);
+			strcat(str, path[i]);
+		}
+		return parseFile(str, assets);
 	}
 
 }
--- BSKT/BSKT.NativeActivity/resources.h
+++ BSKT/BSKT.NativeActivity/resources.h
@@ -4,19 +4,58 @@
 
 #include <GLES2\gl2.h>
 #include "log.h"
+#include <android\asset_manager.h>
+#include <android\asset_manager_jni.h>
+#include <cstring>
+#include <math.h>
+#include <stdlib.h>
 
 #define NULL_RES 0
 
+#define FILE_SEP "/"
+
+#define SHADERS_DIR "shaders"
+#define VERTEX_SHADER_FILE "vertex_shader.glsl"
+#define FRAGMENT_SHADER_FILE "fragment_shader.glsl"
+
+#define MESHES_DIR "meshes"
+#define MESH_VERT_SEP ','
+
 namespace BSKT {
 
-	struct Resources {
-		const GLuint program;
-		Resources(GLuint);
+	struct GLProgramSource {
+		char *vertexShader;
+		char *fragmentShader;
+
+		GLProgramSource();
+
+		GLProgramSource(AAssetManager*, const char*);
+	};
+
+	struct Mesh {
+		GLfloat *vertices;
+		int count;
+
+		Mesh();
+
+		Mesh(AAssetManager*, const char*);
+	};
+
+	struct Assets {
+		GLProgramSource programSource;
+
+		Assets(GLProgramSource);
+	};
+
+	struct GLResources {
+		GLuint program;
+		GLResources(Assets);
 
 		void destroy();
 	};
 
-	Resources createResources();
+	char* parseFile(const char*, AAssetManager*);
+	char* parseFile(const char**, int, AAssetManager*);
 
 }
 #endif
