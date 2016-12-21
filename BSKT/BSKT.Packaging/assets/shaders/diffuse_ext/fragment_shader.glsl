4654fbe45752b10c576cfe750b1c5517c2ba7ec7
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2016-12-21 23:41:07 +0100

    Test basic shadowing

--- BSKT/BSKT.Packaging/assets/shaders/diffuse_ext/fragment_shader.glsl
+++ BSKT/BSKT.Packaging/assets/shaders/diffuse_ext/fragment_shader.glsl
@@ -11,11 +11,14 @@ uniform highp sampler2D u_depthmap;
 varying vec4 v_color;
 varying vec4 v_position;
 
+const float bias = 0.0005;
+
 void main(void) {
-	float smpd = texture2DProj( u_depthmap, v_position.xyw ).z;
 	//float fallof = 1.0 - length(v_position.xy - 0.5);
-	const float bias = 0.0005;
-	float ddif = smpd < (v_position.z-bias) / v_position.w ? 0.0 : 1.0;
-	gl_FragColor = vec4( vec3(ddif * ), 1.0);
+	float bshad = float ( texture2DProj( u_depthmap, v_position.xyw ).z < ( v_position.z - bias ) / v_position.w );
+	vec2 rad = v_position.xy - 0.5;
+	float fallof = 1.0 - dot ( rad, rad ) * 2.0;
+	float w = (1.0 - fallof * bshad) * fallof;
+	gl_FragColor = vec4( vec3(w), 1.0);
 }
 
