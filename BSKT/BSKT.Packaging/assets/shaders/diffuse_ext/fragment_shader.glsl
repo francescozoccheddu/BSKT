87ef13afd78b6f0ec463c55fc601c428f9ee4ca3
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2016-12-22 00:09:31 +0100

    Before blur shader

--- BSKT/BSKT.Packaging/assets/shaders/diffuse_ext/fragment_shader.glsl
+++ BSKT/BSKT.Packaging/assets/shaders/diffuse_ext/fragment_shader.glsl
@@ -12,13 +12,14 @@ varying vec4 v_color;
 varying vec4 v_position;
 
 const float bias = 0.0005;
+const
 
 void main(void) {
 	//float fallof = 1.0 - length(v_position.xy - 0.5);
-	float bshad = float ( texture2DProj( u_depthmap, v_position.xyw ).z < ( v_position.z - bias ) / v_position.w );
+	float hshad = float ( texture2DProj ( u_depthmap, v_position.xyw ).z < ( v_position.z - bias ) / v_position.w );
 	vec2 rad = v_position.xy - 0.5;
 	float fallof = 1.0 - dot ( rad, rad ) * 2.0;
-	float w = (1.0 - fallof * bshad) * fallof;
+	float w = (1.0 - fallof * hshad) * fallof;
 	gl_FragColor = vec4( vec3(w), 1.0);
 }
 
