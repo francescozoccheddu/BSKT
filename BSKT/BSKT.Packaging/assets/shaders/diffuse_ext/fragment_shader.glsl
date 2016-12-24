e836df395c34f6053d12477ac4c66388e143dc7e
francescoz <fz.francescoz@gmail.com>
2016-12-24 16:00:03 +0100

    Test hard shadows

--- BSKT/BSKT.Packaging/assets/shaders/diffuse_ext/fragment_shader.glsl
+++ BSKT/BSKT.Packaging/assets/shaders/diffuse_ext/fragment_shader.glsl
@@ -12,14 +12,11 @@ varying vec4 v_color;
 varying vec4 v_position;
 
 const float bias = 0.0005;
-const
 
 void main(void) {
-	//float fallof = 1.0 - length(v_position.xy - 0.5);
-	float hshad = float ( texture2DProj ( u_depthmap, v_position.xyw ).z < ( v_position.z - bias ) / v_position.w );
+	float hshad = 1.0 -float ( texture2DProj ( u_depthmap, v_position.xyw ).z < ( v_position.z - bias ) / v_position.w );
 	vec2 rad = v_position.xy - 0.5;
 	float fallof = 1.0 - dot ( rad, rad ) * 2.0;
-	float w = (1.0 - fallof * hshad) * fallof;
-	gl_FragColor = vec4( vec3(w), 1.0);
+	gl_FragColor = vec4( vec3(fallof * hshad * v_color.r), 1.0);
 }
 
